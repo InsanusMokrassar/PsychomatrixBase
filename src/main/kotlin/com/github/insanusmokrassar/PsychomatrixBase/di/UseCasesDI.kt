5b0567beaf0a99d12a68142ef63da28deaa446df
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-09-05 20:35:07 +0800

    fill DI by modifyPsychomatrixUseCase

--- src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/di/UseCasesDI.kt
+++ src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/di/UseCasesDI.kt
@@ -1,7 +1,9 @@
 package com.github.insanusmokrassar.PsychomatrixBase.di
 
 import com.github.insanusmokrassar.PsychomatrixBase.domain.UseCases.CalculatePsychomatrixByDateUseCase
+import com.github.insanusmokrassar.PsychomatrixBase.domain.UseCases.ModifyPsychomatrixUseCase
 
 interface UseCasesDI : EntitiesDI {
     val calculatePsychomatrixByDateUseCase: CalculatePsychomatrixByDateUseCase
+    val modifyPsychomatrixUseCase: ModifyPsychomatrixUseCase
 }
--- src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/di/realisations/UseCasesDIImpl.kt
+++ src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/di/realisations/UseCasesDIImpl.kt
@@ -3,13 +3,18 @@ package com.github.insanusmokrassar.PsychomatrixBase.di.realisations
 import com.github.insanusmokrassar.PsychomatrixBase.di.EntitiesDI
 import com.github.insanusmokrassar.PsychomatrixBase.di.UseCasesDI
 import com.github.insanusmokrassar.PsychomatrixBase.domain.UseCases.CalculatePsychomatrixByDateUseCase
+import com.github.insanusmokrassar.PsychomatrixBase.domain.UseCases.ModifyPsychomatrixUseCase
 import com.github.insanusmokrassar.PsychomatrixBase.domain.interactors.CalculatePsychomatrixByDateUseCaseInteractor
+import com.github.insanusmokrassar.PsychomatrixBase.domain.interactors.ModifyPsychomatrixUseCaseInteractor
 
 open class UseCasesDIImpl(
     entitiesDI: EntitiesDI
 ) : UseCasesDI, EntitiesDI by entitiesDI {
-
     override val calculatePsychomatrixByDateUseCase: CalculatePsychomatrixByDateUseCase by lazy {
         CalculatePsychomatrixByDateUseCaseInteractor()
+    }
+
+    override val modifyPsychomatrixUseCase: ModifyPsychomatrixUseCase by lazy {
+        ModifyPsychomatrixUseCaseInteractor()
     }
 }
