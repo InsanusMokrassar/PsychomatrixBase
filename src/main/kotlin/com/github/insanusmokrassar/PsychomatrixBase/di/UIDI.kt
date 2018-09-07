2d2ee91c61202e24d78d0acc99460c1d64c89560
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-09-07 20:28:11 +0800

    remove useless UIDI

--- src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/di/UIDI.kt
+++
@@ -1,7 +0,0 @@
-package com.github.insanusmokrassar.PsychomatrixBase.di
-
-import com.github.insanusmokrassar.PsychomatrixBase.presentation.presenters.DatePickerPresenter
-
-interface UIDI : PresentationLayerDI {
-
-}
