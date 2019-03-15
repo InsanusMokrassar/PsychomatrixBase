9805d66af9890738c3b8af86e45a3aca56f01c29
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-03-15 10:58:15 +0800

    update translations

--- src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/utils/CeilDescriptions/models/CeilInfoConfig.kt
+++ src/main/kotlin/com/github/insanusmokrassar/PsychomatrixBase/utils/CeilDescriptions/models/CeilInfoConfig.kt
@@ -1,13 +1,18 @@
 package com.github.insanusmokrassar.PsychomatrixBase.utils.CeilDescriptions.models
 
 import com.github.insanusmokrassar.PsychomatrixBase.domain.entities.CeilInfo
+import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CeilInfoConfig(
+    @Optional
     val title: String = "",
+    @Optional
     val description: String = "",
+    @Optional
     val note: String? = null,
+    @Optional
     val characteristics: List<CeilCharacteristic> = emptyList()
 ) {
     fun toCeilDescription(countOfCharacteristic: Int): CeilInfo {
